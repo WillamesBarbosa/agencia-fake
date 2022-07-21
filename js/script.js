f3c37ca45e163f0479685192bcf12f511eaf6547
Willames da S. Barbosa <104446605+WillamesBarbosa@users.noreply.github.com>
2022-07-21 12:32:02 -0300

    Atualização do menu mobile
    Correção do bug que era possível rolar a página mesmo com o menu aberto.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 function abrirMenu(){
     let menu = document.getElementById('menuMobile');
     let classeMenu = menu.getAttribute('class');
+    let main = document.querySelector('.main');
+    let section = document.querySelector('.section');
+    let footer = document.querySelector('.footer');
 
     if(classeMenu == "menu"){
         menu.classList.add('Clicado');
+        main.style.display = 'none';
+        section.style.display = 'none';
+        footer.style.display = 'none';
 
     }else{
         menu.classList.remove('Clicado');
+        main.style.display = 'block';
+        section.style.display = 'block';
+        footer.style.display = 'block';
     }
 }
