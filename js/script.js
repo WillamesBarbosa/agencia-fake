ff589af0edf041edf37a75626e1b227487974680
Willames da S. Barbosa <104446605+WillamesBarbosa@users.noreply.github.com>
2023-03-07 16:54:31 -0300

    refactor: menu mobile updated

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 function abrirMenu(){
     let menu = document.getElementById('menuMobile');
     let classeMenu = menu.getAttribute('class');
-    let main = document.querySelector('.main');
-    let section = document.querySelector('.section');
-    let footer = document.querySelector('.footer');
+    let body = document.querySelector('.body');
 
     if(classeMenu == "menu"){
         menu.classList.add('Clicado');
-        main.style.display = 'none';
-        section.style.display = 'none';
-        footer.style.display = 'none';
+        body.style.overflow = 'hidden';
+        window.scrollTo(0, 0);
 
     }else{
         menu.classList.remove('Clicado');
-        main.style.display = 'block';
-        section.style.display = 'block';
-        footer.style.display = 'block';
+        body.style.overflow = 'visible';
     }
 }
